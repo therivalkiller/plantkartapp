3c901281ccc49ea349bfd9e403084e1fe3c7b90d
therivalkiller <amritanshu3011@gmail.com>
2022-07-01 00:35:01 +0530

    prepare

---
+++ backend/data/products.js
@@ -0,0 +1,89 @@
+const products = [
+  {
+    name: 'Rose (Any Colour)',
+    image: '/images/rose.jpg',
+    description:
+      'rose, (genus Rosa), genus of some 100 species of perennial shrubs in the rose family (Rosaceae). Roses are native primarily to the temperate regions of the Northern Hemisphere. Many roses are cultivated for their beautiful flowers, which range in colour from white through various tones of yellow and pink to dark crimson and maroon, and most have a delightful fragrance, which varies according to the variety and to climatic conditions.',
+    brand: 'genus Rosa',
+    category: 'Flowering',
+    price: 99,
+    countInStock: 10,
+    rating: 4.5,
+    numReviews: 12,
+    nurseryName: "Ashiana Nagar",
+    pincode: "800025"
+
+  },
+  {
+    name: 'Jasmine',
+    image: '/images/jasmine.jpg',
+    description:
+      'jasmine, (genus Jasminum), also spelled jessamine, genus of about 200 species of fragrant-flowered shrubs and vines of the olive family (Oleaceae). The plants are native to tropical and to some temperate areas of the Old World. Several are cultivated as ornamentals. The leaves can be evergreen or deciduous and usually are composed of two or more leaflets, although some species have simple leaves. The fruit in most species is a two-lobed black berry.',
+    brand: 'Jasminum',
+    category: 'Flowering',
+    price: 99,
+    countInStock: 7,
+    rating: 4.0,
+    numReviews: 8,
+    nurseryName: "Ashiana Nagar",
+    pincode: "800025"
+  },
+  {
+    name: 'Orchid',
+    image: '/images/orchid.jpg',
+    description:
+      'orchid, (family Orchidaceae), any of nearly 1,000 genera and more than 25,000 species of attractively flowered plants distributed throughout the world, especially in wet tropics. Orchidaceae is a member of Asparagales, an order of monocotyledonous flowering plants that also includes the asparagus and iris families. The word orchid is derived from the Greek word (orchis) for testicle because of the shape of the root tubers in some species of the genus Orchis. These nonwoody perennial plants are generally terrestrial or epiphytic herbs (i.e., growing on other plants rather than rooted in soil). Those attached to other plants often are vinelike and have a spongy root covering called the velamen that absorbs water from the surrounding air. Most species manufacture their own food, but some live on dead organic material (saprophytic) or are helped to obtain nourishment by a fungus living in their roots.',
+    brand: 'Orchidaceae',
+    category: 'Flowering',
+    price: 99,
+    countInStock: 5,
+    rating: 3,
+    numReviews: 12,
+    nurseryName: "Ashiana Nagar",
+    pincode: "800025"
+  },
+  {
+    name: 'Gooseberry (Amla)',
+    image: '/images/gooseberry.jpg',
+    description:
+      'gooseberry, any of several species of flowering shrubs of the genus Ribes (family Grossulariaceae), cultivated for their edible fruits and as ornamentals. Currants and gooseberries form two groups within the genus Ribes; some taxonomic systems formerly placed gooseberries in their own genus, Grossularia. The tart fruit is eaten ripe and is often made into jellies, preserves, pies and other desserts, or wine.',
+    brand: 'Ribes',
+    category: 'Medicinal',
+    price: 149,
+    countInStock: 11,
+    rating: 5,
+    numReviews: 12,
+    nurseryName: "Ashiana Nagar",
+    pincode: "800025"
+  },
+  {
+    name: 'Tulsi (Holy basil)',
+    image: '/images/tulsi.jpg',
+    description:
+      'holy basil, (Ocimum tenuiflorum), also called tulsi or tulasi, flowering plant of the mint family (Lamiaceae) grown for its aromatic leaves. Holy basil is native to the Indian subcontinent and grows throughout Southeast Asia. The plant is widely used in Ayurvedic and folk medicine, often as an herbal tea for a variety of ailments, and is considered sacred in Hinduism.',
+    brand: 'Lamiacaea',
+    category: 'Medicinal',
+    price: 149,
+    countInStock: 7,
+    rating: 4.5,
+    numReviews: 10,
+    nurseryName: "Ashiana Nagar",
+    pincode: "800025"
+  },
+  {
+    name: 'Snake Plant',
+    image: '/images/snakeplant.jpg',
+    description:
+      'Mother-in-laws tongue, or snake plant (Dracaena trifasciata, formerly Sansevieria trifasciata), is a popular houseplant with yellow-striped leaves and tiny pale green scented flowers. It is sometimes sold as Sansevieria. Sansevieria, former genus of about 70 species of flowering plants in the asparagus family (Asparagaceae), now placed in the genus Dracaena based on phylogenetic data. The group is native primarily to tropical Africa. Many species have water-resistant leaf fibres that are sometimes used in the manufacture of ropes and for bowstrings, and several are grown as ornamentals for their attractive foliage.',
+    brand: 'Amazon',
+    category: 'Flowering (Decorative)',
+    price: 199,
+    countInStock: 0,
+    rating: 4,
+    numReviews: 12,
+    nurseryName: "Ashiana Nagar",
+    pincode: "800025"
+  },
+]
+
+export default products
